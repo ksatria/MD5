2dfd98270e5be2f711563746f033c9ee4e328028
ksatria <khoirul.umam35@gmail.com>
2012-12-12 22:12:29 +0700

    Pengubahan tampilan interface
    - Menambahkan style pada tampilan dengan menggunakan Twitter Bootstrap
    v2.2.2
    - Mengubah file index.php

--- index.php
+++ index.php
@@ -1,22 +1,45 @@
 <html>
 	<head>
 		<title>Tes MD5</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+		<style type="text/css">
+			body{
+				margin-top: 50px;
+			}
+			
+			#plain-text{
+				font-weight: bold;
+				font-size: 17px;
+			}
+			
+			div.center{
+				text-align: center;
+			}
+		</style>
 	</head>
 	<body>
-		<form method="post" action="#">
-			<label>Masukkan kalimat yang akan dienkripsikan</label><br>
-			<input type="text" name="input">
-			<input type="submit" value="Proses">
-		</form>
-		
-		<?PHP
-			$input = $_POST["input"];
-			$hasil = md5($input);
-			
-			if(isset($input)){
-				echo "Hasil enkripsi :<br>";
-				echo "$hasil";
-			}
-		?>
+		<div class="container">
+			<div class="hero-unit">
+				<h1>MD5 Encryptor</h1>
+				<p>Aplikasi enkripsi MD5 sederhana</p>
+				<p><small>Oleh <a href="http://www.facebook.com/umam.ksatria" target="_blank">Khoirul Umam</a></small></p>
+			</div>
+			<div class="center">
+				<form method="post">
+					<input type="text" name="input" id="plain-text" class="input-block-level" placeholder="Masukkan teks yang akan dienkripsi di sini...">
+					<input type="submit" value="Enkripsi" class="btn btn-primary btn-large">
+				</form>
+				
+				<?PHP
+					$input = $_POST["input"];
+					$hasil = md5($input);
+					
+					if(isset($input)){
+						echo "<p class=\"lead\">Hasil enkripsi <strong>".$input."</strong>:</p>";
+						echo "<div class=\"alert alert-info lead\">".$hasil."</div>";
+					}
+				?>
+			</div>
+		</div>
 	</body>
 </html>
